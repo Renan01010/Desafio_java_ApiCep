496cf546f3792a1da51b93c81fb00a2c43d6e55c
Renan Teles <renantelesdeoliveira@gmail.com>
2026-08-18 18:00:53 -0300

    Implementando a API CEPs

--- src/br/ufpr/modulos/principal/Principal.java
+++ src/br/ufpr/modulos/principal/Principal.java
@@ -1,7 +1,42 @@
 package br.ufpr.modulos.principal;
 
-public class Principal {
-    static void main() {
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Principal {
+    static void main() throws IOException, InterruptedException {
+        Scanner scn = new Scanner(System.in);
+            System.out.println("Entre com um CEP: ");
+            int cep = scn.nextInt();
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        String url = "https://viacep.com.br/ws/"+cep+"/json/";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
+        FileWriter escrita = new FileWriter("Ceps.json");
+        escrita.write(gson.toJson(json));
+        escrita.close();
     }
 }
